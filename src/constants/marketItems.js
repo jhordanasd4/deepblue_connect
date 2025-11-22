8a15169ece1961f2c5130c558e869a6dcdcaa99b
jhordan <jhordancitopro24@gmail.com>
2025-11-22 04:50:50 -0500

    FIX: Implementa diseño responsive para vista movil.

--- src/constants/marketItems.js
+++ src/constants/marketItems.js
@@ -1,7 +1,7 @@
 // Lista centralizada de ítems del mercado para evitar duplicación entre componentes
 const marketItems = [
-  { id: 'coral', name: 'Coral Brillante', cost: 50, reward: '+2 usdt diario', description: 'Los usdt se agregaran automaticamente desde la compra(cada 24h).', image: '/img/coral.jpg' },
-  { id: 'alga', name: 'Alga Energética', cost: 90, reward: '+4 usdt diario', description: 'Los usdt se agregaran automaticamente desde la compra(cada 24h).', image: '/img/alga.avif' },
+  { id: 'coral', name: 'Coral Brillante', cost: 50, reward: '+2 usdt diario', description: 'Los usdt se agregaran automaticamente a las 12pm.', image: '/img/coral.jpg' },
+  { id: 'alga', name: 'Alga Energética', cost: 90, reward: '+4 usdt diario', description: 'Los usdt se agregaran automaticamente a las 12pm', image: '/img/alga.avif' },
 ];
 
 export default marketItems;
